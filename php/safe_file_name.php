531aba67745d1d65e94d365e014c384f804dcd22
Jishnu <jishnu7@gmail.com>
2011-12-03 16:26:27 +0530

    converted to function;

--- php/safe_file_name.php
+++ php/safe_file_name.php
@@ -1,9 +1,12 @@
-$SafeFile = $HTTP_POST_FILES['ufile']['name'];
-$SafeFile = str_replace("#", "No.", $SafeFile);
-$SafeFile = str_replace("$", "Dollar", $SafeFile);
-$SafeFile = str_replace("%", "Percent", $SafeFile);
-$SafeFile = str_replace("^", "", $SafeFile);
-$SafeFile = str_replace("&", "and", $SafeFile);
-$SafeFile = str_replace("*", "", $SafeFile);
-$SafeFile = str_replace("?", "", $SafeFile); 
-
+function safe_name($name)
+{
+	$SafeFile = $name; //$HTTP_POST_FILES['file']['name'];
+	$SafeFile = str_replace("#", "No.", $SafeFile);
+	$SafeFile = str_replace("$", "Dollar", $SafeFile);
+	$SafeFile = str_replace("%", "Percent", $SafeFile);
+	$SafeFile = str_replace("^", "", $SafeFile);
+	$SafeFile = str_replace("&", "and", $SafeFile);
+	$SafeFile = str_replace("*", "", $SafeFile);
+	$SafeFile = str_replace("?", "", $SafeFile);
+	return $SafeFile;
+}
