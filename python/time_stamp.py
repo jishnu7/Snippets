2ad5c1a772250849056a6c9e14b3ae82e98d2581
Jishnu <jishnu7@gmail.com>
2012-06-05 22:21:45 +0530

    import datetime

--- python/time_stamp.py
+++ python/time_stamp.py
@@ -3,6 +3,7 @@ Generate unix time stamp.
 Pass number of days/time to add
 '''
 
+import datetime
 def timestamp():
     days=0,minutes=0,seconds=0, 
     date = datetime.datetime.now()+datetime.timedelta(days,minuites,seconds)
