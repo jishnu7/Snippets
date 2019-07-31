d4793ac84bb46c4fe3d4a5f7e14673de772c6b19
Jishnu Mohan <jishnu7@gmail.com>
2019-07-31 19:16:57 +0530

    update jul 31

--- ad-hoc/3g.sh
+++ ad-hoc/3g.sh
@@ -3,8 +3,8 @@ iptables --table nat --flush
 iptables --delete-chain
 iptables --table nat --delete-chain
 
-ifconfig eth0 192.168.0.254
+ifconfig wlan0 10.42.0.254
 echo 1 > /proc/sys/net/ipv4/ip_forward
 systemctl restart dnsmasq
-iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
